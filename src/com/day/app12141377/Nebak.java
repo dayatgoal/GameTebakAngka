46b1c8317c0ce51c931fdaec05efcfcca359543e
Dayat Computer <Dayat Computer@DayatComputer>
2016-11-22 19:48:17 +0700

    12141377 System.out.println("# Level " + PiLevel); digunakan untuk menaikan level berikutnya, pang=PilLevel*100 digunakan untuk menaikan nilai dan angka random pada tiap level, System.out.println digunakan untuk menampilkan perintah menebak anata 1-100 dan memberitahu bahwa player mempunyai 5 kesempatan,int Angkaacak=angkarandom.nextInt digunakan untuk mengacak nomer mulai looping 5 kali.

--- src/com/day/app12141377/Nebak.java
+++ src/com/day/app12141377/Nebak.java
@@ -26,3 +26,11 @@ public class Nebak {
                 int PiLevel=1;   // Untuk menentukan Level Pertama dan selanjutnya akan dinaikkan nilainya 
                 System.out.println("Selamat Datang " + cariY); // Untuk memberikan sambutan kepada Pemain
                 do{
+                int Kout, Ang, Pang;
+                    System.out.println("# Level " + PiLevel);
+                    int StarNilaiO=PiLevel-1;    
+                    Pang=PiLevel*100; // Menaiikan Nilai dan Angka Random pada tiap level
+                    System.out.println("Silahkan Tebak Antara 1 s/d " + Pang);
+                    System.out.println("Anda mempunyai kesempatan menebak 5 kali.");
+                    int AngKaAcak=angkaRandom.nextInt(Pang); // Mulai mengacak Nomor
+                    // Mulai Looping 5 kali kesempatan. disni malah 6 kali, karena yg ke 6 untuk memberikan variabel GameOver
