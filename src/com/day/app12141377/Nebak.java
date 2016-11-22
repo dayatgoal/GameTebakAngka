63a1c88cde373581131446ba49bbb535b87f0435
Dayat Computer <Dayat Computer@DayatComputer>
2016-11-22 19:18:24 +0700

    12141377 "ArrayList <String>" digunakan untuk membuat array Nama Pemain," ArrayList <Integer>" digunakan untuk membuat array sekor pemain," Scanner Input" digunakan untuk menginput kan angka, random digunakan untuk mengacak angka, "String cariY" untuk mencari nilai Y,

--- src/com/day/app12141377/Nebak.java
+++ src/com/day/app12141377/Nebak.java
@@ -2,3 +2,12 @@ package com.day.app12141377;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
+public class Nebak {
+    public static void main(String[] args) {
+        ArrayList <String> namaPlayer = new ArrayList<>(); // Membuat array Nama Pemain
+        ArrayList <Integer> nilaiPermainan = new ArrayList<>(); // Membuat array Sekor Pemain
+        Scanner input = new Scanner(System.in); // Input pertama
+        Scanner input2= new Scanner(System.in); // Input kedua
+        Random angkaRandom=new Random(); // Mengacak angka
+        String cariY; // Untuk mencari nilai Y, jika input y, maka otomatis akan keluar
+        do{
